0cee4f7373b6f842cc8dbb650533227ffc60173c
readypeng <38313898+readypeng@users.noreply.github.com>
2020-08-20 09:32:15 +0800

    d

---
+++ proj0/myfiles/Body.java
@@ -0,0 +1,21 @@
+/**
+ * Body
+ */
+public class Body {
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public double yyPos;
+    public String imgFileName;
+
+    public Body(double xP, double yP, double xV, double yV, double m, String img) {
+
+    }
+
+    public Body(Body b){
+
+    }
+
+    /*all methods should be non-static.*/
+
+}
